00eb601d91eede2533a6e27e090664f9b4e66f76
KasumiMercury <naoki_shira@outlook.jp>
2024-11-28 22:15:21 +0900

    chore: add biome

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 ![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white)
 ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
 
+[![Formatted with Biome](https://img.shields.io/badge/Formatted_with-Biome-60a5fa?style=flat&logo=biome)](https://biomejs.dev/)
+
 [@crxjs/vite-plugin](https://github.com/crxjs/chrome-extension-tools)
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
-import './App.css'
-import {useState} from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-    const [urlState, setURL] = useState('')
+	const [urlState, setURL] = useState("");
 
-    // get current tab's url with chrome.tabs.query
-    const getCurrentTabURL = () => {
-        console.log('getTabURL')
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            const tab = tabs[0]
-            const url = tab.url
-            console.log(url)
-            if (!url) {
-                return
-            }
-            setURL(url)
-        })
-    }
+	// get current tab's url with chrome.tabs.query
+	const getCurrentTabURL = () => {
+		console.log("getTabURL");
+		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+			const tab = tabs[0];
+			const url = tab.url;
+			console.log(url);
+			if (!url) {
+				return;
+			}
+			setURL(url);
+		});
+	};
 
-    return (
-        <>
-            <div>
-                <button onClick={getCurrentTabURL}>GetParsedURL</button>
-                <div>{urlState}</div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div>
+				<button type="button" onClick={getCurrentTabURL}>
+					GetParsedURL
+				</button>
+				<div>{urlState}</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
