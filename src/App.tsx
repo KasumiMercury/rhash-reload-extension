c045593969d5d746fcdbd44549616f1cc1cedffc
KasumiMercury <naoki_shira@outlook.jp>
2024-11-28 19:00:05 +0900

    feat: get current tab url

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [urlState, setURL] = useState('')
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // get current tab's url with chrome.tabs.query
+    const getCurrentTabURL = () => {
+        console.log('getTabURL')
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            const tab = tabs[0]
+            const url = tab.url
+            console.log(url)
+            if (!url) {
+                return
+            }
+            setURL(url)
+        })
+    }
+
+    return (
+        <>
+            <div>
+                <button onClick={getCurrentTabURL}>GetParsedURL</button>
+                <div>{urlState}</div>
+            </div>
+        </>
+    )
 }
 
 export default App
