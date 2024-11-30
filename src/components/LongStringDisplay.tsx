a445573230203897bc470e27e0ef56b5ed99542d
KasumiMercury <naoki_shira@outlook.jp>
2024-11-30 20:27:31 +0900

    refactor: tip hover

--- src/components/LongStringDisplay.tsx
+++ src/components/LongStringDisplay.tsx
@@ -1,9 +1,9 @@
 export default function LongStringDisplay({ text }: { text: string }) {
 	return (
-		<div className="w-full h-fit cursor-pointer relative group">
-			<p className="w-full h-fit truncate overflow-x-hidden">{text}</p>
+		<div className="w-full h-fit cursor-pointer relative">
+			<p className="w-full h-fit truncate overflow-x-hidden peer">{text}</p>
 
-			<div className="hidden group-hover:block absolute h-fit max-w-full bottom-0 left-1/2 -translate-x-1/2 translate-y-full bg-zinc-900 z-10 border-zinc-50 border text-left break-words p-2 rounded-md">
+			<div className="hidden peer-hover:block absolute h-fit max-w-full bottom-0 left-1/2 -translate-x-1/2 translate-y-full bg-zinc-900 z-10 border-zinc-50 border text-left break-words p-2 rounded-md">
 				{text}
 			</div>
 		</div>
