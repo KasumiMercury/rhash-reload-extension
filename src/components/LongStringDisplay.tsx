119bb36e3b7c7f0fce933f4f59c1c382b11b9383
KasumiMercury <naoki_shira@outlook.jp>
2024-11-30 20:32:25 +0900

    feat: style

--- src/components/LongStringDisplay.tsx
+++ src/components/LongStringDisplay.tsx
@@ -1,9 +1,9 @@
 export default function LongStringDisplay({ text }: { text: string }) {
 	return (
-		<div className="w-full h-fit cursor-pointer relative">
-			<p className="w-full h-fit truncate overflow-x-hidden peer">{text}</p>
+		<div className="w-full h-fit cursor-pointer relative text-left py-1 px-2 rounded-md border border-zinc-300">
+			<p className="w-full h-fit truncate overflow-x-hidden peer select-all">{text}</p>
 
-			<div className="hidden peer-hover:block absolute h-fit max-w-full bottom-0 left-1/2 -translate-x-1/2 translate-y-full bg-zinc-900 z-10 border-zinc-50 border text-left break-words p-2 rounded-md">
+			<div className="hidden peer-hover:block absolute h-fit max-w-full bottom-0 left-1/2 -translate-x-1/2 translate-y-full bg-zinc-900 z-10 border-zinc-300 border text-left break-words p-2 rounded-md">
 				{text}
 			</div>
 		</div>
