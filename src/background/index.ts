b7a0f75707dc84f4c1df2b22680131be1dab767f
KasumiMercury <naoki_shira@outlook.jp>
2024-11-30 22:01:43 +0900

    feat: hash remove reload by command

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,12 @@
+import { getActiveTabInfo, getHashRemovedURL } from "../utils/url.ts";
+
 chrome.commands.onCommand.addListener((command) => {
 	switch (command) {
-		case "remove_hash_reload": // 設定したショートカット名
-			console.log("Run");
+		case "remove_hash_reload":
+			getActiveTabInfo().then((info) => {
+				console.log("Current URL", info.url);
+				chrome.tabs.update({ url: getHashRemovedURL(info.url) });
+			});
 			break;
 	}
 });
